7f5952ac69ffef33f3a93f9db03681052bba3bab
Graham Lyus <graham.lyus@focusatwill.com>
2019-05-21 15:25:54 -0700

    Support Rails 5.2+
    Change redirect_to :back to redirect_back to  (fixes undefined method `back_url')

--- lib/active_admin/acts_as_list/helper.rb
+++ lib/active_admin/acts_as_list/helper.rb
@@ -50,42 +50,42 @@ module ActiveAdmin
       def sortable_member_actions
        member_action :move_to_top do
           if resource.first?
-            redirect_to :back, :notice => I18n.t('acts_as_list.illegal_move_to_top', :resource => resource_class.to_s.camelize.constantize.model_name.human ) 
+            redirect_back fallback_location: active_admin_root, notice: I18n.t('acts_as_list.illegal_move_to_top', :resource => resource_class.to_s.camelize.constantize.model_name.human ) 
             return
           end  
           
           resource.move_to_top
-          redirect_to :back, :notice => I18n.t('acts_as_list.moved_to_top', :resource => resource_class.to_s.camelize.constantize.model_name.human )
+          redirect_back fallback_location: active_admin_root, notice: I18n.t('acts_as_list.moved_to_top', :resource => resource_class.to_s.camelize.constantize.model_name.human )
         end
         
         member_action :move_to_bottom do
           if resource.last?
-            redirect_to :back, :notice => I18n.t('acts_as_list.illegal_move_to_bottom', :resource => resource_class.to_s.camelize.constantize.model_name.human ) 
+            redirect_back fallback_location: active_admin_root, notice: I18n.t('acts_as_list.illegal_move_to_bottom', :resource => resource_class.to_s.camelize.constantize.model_name.human ) 
             return
           end  
           
           resource.move_to_bottom
-          redirect_to :back, :notice => I18n.t('acts_as_list.moved_to_bottom', :resource => resource_class.to_s.camelize.constantize.model_name.human )
+          redirect_back fallback_location: active_admin_root, notice: I18n.t('acts_as_list.moved_to_bottom', :resource => resource_class.to_s.camelize.constantize.model_name.human )
         end
 
         member_action :move_up do
           if resource.first?
-            redirect_to :back, :notice => I18n.t('acts_as_list.illegal_move_up', :resource => resource_class.to_s.camelize.constantize.model_name.human ) 
+            redirect_back fallback_location: active_admin_root, notice: I18n.t('acts_as_list.illegal_move_up', :resource => resource_class.to_s.camelize.constantize.model_name.human ) 
             return
           end  
           
           resource.move_higher
-          redirect_to :back, :notice => I18n.t('acts_as_list.moved_up', :resource => resource_class.to_s.camelize.constantize.model_name.human )
+          redirect_back fallback_location: active_admin_root, notice: I18n.t('acts_as_list.moved_up', :resource => resource_class.to_s.camelize.constantize.model_name.human )
         end
 
         member_action :move_down do
           if resource.last?
-            redirect_to :back, :notice => I18n.t('acts_as_list.illegal_move_down', :resource => resource_class.to_s.camelize.constantize.model_name.human ) 
+            redirect_back fallback_location: active_admin_root, notice: I18n.t('acts_as_list.illegal_move_down', :resource => resource_class.to_s.camelize.constantize.model_name.human ) 
             return
           end  
           
           resource.move_lower
-          redirect_to :back, :notice => I18n.t('acts_as_list.moved_down', :resource => resource_class.to_s.camelize.constantize.model_name.human )
+          redirect_back fallback_location: active_admin_root, notice: I18n.t('acts_as_list.moved_down', :resource => resource_class.to_s.camelize.constantize.model_name.human )
         end    
       end
     end
